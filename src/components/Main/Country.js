57861c6ae5ca089f44b8bd6a3d7249d8ec271824
Siddharth Bhatnagar <siddharthb1604@gmail.com>
2021-10-20 00:18:28 +0530

    made changes according to API version changes

--- src/components/Main/Country.js
+++ src/components/Main/Country.js
@@ -1,20 +1,20 @@
 import React from "react";
 
 const Country = ({country}) => {
-
-  let border = "";
-  for(let i=0;i<country.borders.length;i++){
-    if(i!==0){
-      border += ', ';
-    }
-    border += `${country.borders[i]}`;  
-  }
+  //console.log(country);
+  // let border = "";
+  // for(let i=0;i<country.borders.length;i++){
+  //   if(i!==0){
+  //     border += ', ';
+  //   }
+  //   border += `${country.borders[i]}`;  
+  // }
 
   return ( 
     <div className="card grow" >
-      <img src={country.flag} className="card-img-top" alt="flag"></img>
+      <img src={country.flags.png} className="card-img-top" alt="flag"></img>
       <div className="card-body">
-        <h3 className="card-title text-center">{country.name}</h3>
+        <h3 className="card-title text-center">{country.name.common}</h3>
         <p className="card-text">Capital: </p>
         <p className="text-center">{country.capital}</p>
         <p className="card-text">Region: </p>
@@ -23,10 +23,10 @@ const Country = ({country}) => {
         <p className="text-center">{country.subregion}</p>
         <p className="card-text">Population: </p>
         <p className="text-center">{country.population}</p>
-        <p className="card-text">Border: </p>
-        <p className="text-center">{border}</p>
-        <p className="card-text">Languages:</p> 
-        <p className="text-center">{country.languages[0]['name']}&nbsp;({country.languages[0]['nativeName']})</p>
+        {/* <p className="card-text">Border: </p> */}
+        {/* <p className="text-center">{border}</p> */}
+        {/* <p className="card-text">Languages:</p> 
+        <p className="text-center">{country.languages[0]['name']}&nbsp;({country.languages[0]['nativeName']})</p> */}
       </div>
     </div>
   );
