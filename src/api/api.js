84ee06c27028a2d6cd495d1431469a0ebcb92cf8
Puneet Goel <puneetgoel016@gmail.com>
2021-10-20 14:20:29 +0530

    api updated, countries sorted, css bugs

--- src/api/api.js
+++ src/api/api.js
@@ -2,9 +2,20 @@ import axios from "axios";
 
 const apiUrl = "https://restcountries.com/v3.1/region/";
 
+const comparator = (a,b) => {
+  if(a.name.common < b.name.common){
+    return -1;
+  }
+  if(a.name.common > b.name.common){
+    return 1;
+  }
+  return 0;
+}
+
 const getCountries = async(continent,changeData) => {
     try{
       let {data} = await axios.get(apiUrl + continent);
+      data.sort(comparator);
       changeData(data);
     }catch(error){
       console.log(error);
--- src/components/Main/Country.js
+++ src/components/Main/Country.js
@@ -1,32 +1,36 @@
 import React from "react";
 
 const Country = ({country}) => {
-  //console.log(country);
-  // let border = "";
-  // for(let i=0;i<country.borders.length;i++){
-  //   if(i!==0){
-  //     border += ', ';
-  //   }
-  //   border += `${country.borders[i]}`;  
-  // }
+
+  //in case there are no borders then []
+  let border = (country.borders || []).join(', ');
+  let currency = country.currencies[Object.keys(country.currencies)[0]];
 
   return ( 
     <div className="card grow" >
       <img src={country.flags.png} className="card-img-top" alt="flag"></img>
       <div className="card-body">
-        <h3 className="card-title text-center">{country.name.common}</h3>
-        <p className="card-text">Capital: </p>
+        <h3 className="card-title text-center fw-bold">{country.name.common}</h3>
+        <p className="card-text fst-italic fw-light">Capital: </p>
         <p className="text-center">{country.capital}</p>
-        <p className="card-text">Region: </p>
-        <p className="text-center">{country.region}</p>
-        <p className="card-text">Sub-Region: </p>
+        <p className="card-text fst-italic fw-light">Currency: </p> 
+        <p className="text-center">{currency.name} ( {currency.symbol} )</p> 
+        <p className="card-text fst-italic fw-light">Sub-Region: </p>
         <p className="text-center">{country.subregion}</p>
-        <p className="card-text">Population: </p>
+        <p className="card-text fst-italic fw-light">Population: </p>
         <p className="text-center">{country.population}</p>
-        {/* <p className="card-text">Border: </p> */}
-        {/* <p className="text-center">{border}</p> */}
-        {/* <p className="card-text">Languages:</p> 
-        <p className="text-center">{country.languages[0]['name']}&nbsp;({country.languages[0]['nativeName']})</p> */}
+        {
+          (border === '')?<div />:
+          <div>
+            <p className="card-text fst-italic fw-light">Border: </p>
+            <p className="text-center">{border}</p>
+          </div>
+        }
+        {
+          (!country.independent)?<div />:
+          <p className="text-center fst-italic fw-bold">Independent</p>
+        }
+        
       </div>
     </div>
   );
